ac08cd6fb1e407c359032f52a5bb82f5f42d2317
iam <brian.kibet2@student.moringaschool.com>
2023-06-25 18:26:26 +0000

    final submit

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # phase-1-code-challange-2
+
+ - Vote for the Cutest Animal
+This is a simple web application that allows users to vote for the cutest animal. Users can see a list of animal names, view the details of each animal, and vote for their favorite animal.
+## Features
+    • Fetches animal data from a server
+    • Displays a list of animal names
+    • Shows the details of each animal when clicked
+    • Allows users to vote for their favorite animal
+    • Updates the vote count dynamically
+## Technologies Used
+    • HTML
+    • CSS (with style.css file)
+    • JavaScript
+## Getting Started
+    1. Clone the repository: git clone <repository-url>
+    2. Navigate to the project directory: cd flataculties
+    3. Open the index.html file in a web browser.
+## Usage
+    • Upon loading the application, the list of animal names will be displayed.
+    • Click on an animal name to view its details in the animal details section.
+    • Enter the number of votes in the input field and click the "Vote" button to update the vote count.
+## Data Source
+The animal data is fetched from a server using the following endpoint: http://localhost:3000/characters. Make sure the server is running and providing the required data in the expected format.
+
+
+## AUTHOR
+BRIAN KIBET
+
+## Acknowledgements
+    • This project is for educational purposes and is inspired by various web development tutorials and examples.
+Contributing
+Contributions are welcome! If you find any issues or have suggestions for improvement, please open an issue or submit a pull request.
+
+
+## LICENSE
+Copyright © 2023 BRIAN KIBET. All rights reserved.
+
+The code, design, and content of this project are the exclusive property of Your Company Name and are protected by copyright laws. Any unauthorized use, reproduction, or distribution is strictly prohibited.
+
+For inquiries, please contact:
+
+## BRIAN KIBET
+Email: brian.kibet2@student.moringaschool.com
+
+
--- index.js
+++ index.js
@@ -1,30 +1,44 @@
-const animalNameElement = document.getElementById('animal-name');
-const animalImageElement = document.getElementById('animal-image');
-const animalVotesElement = document.getElementById('animal-votes');
-const resetButton = document.getElementById('reset-button');
 
-// Reset button event listener
-resetButton.addEventListener('click', () => {
-  resetVotes();
-});
+document.addEventListener('DOMContentLoaded', () => {
+  // Fetch animal data from the server
+  fetch('http://localhost:3000/characters')
+    .then(response => response.json())
+    .then(data => {
+      const animalNamesList = document.getElementById('animal-names');
+      const animalDetailsContainer = document.getElementById('animal-details');
 
-// Function to reset votes
-function resetVotes() {
-  // Perform the necessary logic to reset the votes
-  // For example, you can send a PUT request to update the votes to 0 on the server
-  
-  // Update the UI to reflect the reset
-  animalVotesElement.textContent = 'Votes: 0';
-}
+      // show the animal names list
+      data.forEach(animal => {
+        const listItem = document.createElement('li');
+        listItem.textContent = animal.name;
+        listItem.addEventListener('click', () => {
+          // Display animal details when clicked
+          displayAnimalDetails(animal);
+        });
+        animalNamesList.appendChild(listItem);
+      });
 
-// Fetch animal details and votes from the server and update the UI
-fetch('http://localhost:3000/characters/1') 
-  .then(response => response.json())
-  .then(animal => {
-    animalNameElement.textContent = animal.name;
-    animalImageElement.src = animal.image;
-    animalVotesElement.textContent = `Votes: ${animal.votes}`;
-  })
-  .catch(error => {
-    console.log('Error fetching animal details:', error);
+     
+    });
+
+  // Function to display animal details
+  function displayAnimalDetails(animal) {
+    const animalDetailsContainer = document.getElementById('animal-details');
+    animalDetailsContainer.innerHTML = ` 
+      <img src="${animal.image}" alt="${animal.name}">
+      <p>Votes: ${animal.votes}</p>`
+    ;
+  }
+
+  // Vote button click event
+  const voteButton = document.getElementById('vote-button');
+  voteButton.addEventListener('click', () => {
+    const voteInput = document.getElementById('vote-input');
+    const votes = parseInt(voteInput.value, 10);
+    if (!isNaN(votes)) {
+      const animalDetailsContainer = document.getElementById('animal-details');
+      const votesParagraph = animalDetailsContainer.querySelector('p');
+      votesParagraph.textContent = `Votes: ${votes}`;
+    }
   });
+});
